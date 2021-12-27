2e87e04b3dc8be76f2ac6a2e29930896ce359104
chrisdaid <c.chrisdb@gmail.com>
2021-12-27 17:57:52 -0500

    tooltip logic added

--- minerstats.js
+++ minerstats.js
@@ -1,10 +1,29 @@
 const copyBtn = document.querySelector(".copy-btn");
 const copyBtnTooltip = document.querySelector(".copy-btn-tt");
+const addBtn = document.querySelector(".add-btn");
+const addBtnTooltip = document.querySelector(".add-btn-tt");
+
 copyBtn.addEventListener("mouseover", () => {
   copyBtnTooltip.innerText = "Copy";
-  copyBtnTooltip.classList.add("visible");
 });
 
-copyBtn.addEventListener("mouseleave", () => {
-  copyBtnTooltip.classList.remove("visible");
+addBtn.addEventListener("mouseover", () => {
+  if (addBtnTooltip.innerText === "Added") {
+    addBtnTooltip.innerText = "Remove";
+  } else {
+    addBtnTooltip.innerText = "Add";
+  }
 });
+
+addBtn.addEventListener("click", () => {
+  if (
+    addBtnTooltip.innerText === "Remove" ||
+    addBtnTooltip.innerText === "Removed"
+  ) {
+    addBtnTooltip.innerText = "Removed";
+  } else {
+    addBtnTooltip.innerText = "Added";
+  }
+});
+
+copyBtn.addEventListener("mouseleave", () => {});
