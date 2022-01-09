a65b2451c6cbb4cc204c3b1ca91d5cb94b804e9d
chrisdaid <c.chrisdb@gmail.com>
2022-01-09 12:40:30 -0500

    refactored truncate method

--- truncate.js
+++ truncate.js
@@ -1,27 +1,9 @@
 const truncToTwo = (num) => {
-  num = num.toString().split("");
-  let indexOfDot = num.indexOf(".");
-  let placeholder = Math.trunc(num.join(""));
-  placeholder = String(placeholder).split("");
-  placeholder.push(num[indexOfDot]);
-  placeholder.push(num[indexOfDot + 1]);
-  placeholder.push(num[indexOfDot + 2]);
-  return placeholder.join("");
+  return num.toFixed(2);
 };
 
 const truncToFive = (num) => {
-  num = num.toString().split("");
-  let indexOfDot = num.indexOf(".");
-  let placeholder = Math.trunc(num.join(""));
-  placeholder = String(placeholder).split("");
-  placeholder.push(num[indexOfDot]);
-  placeholder.push(num[indexOfDot + 1]);
-  placeholder.push(num[indexOfDot + 2]);
-  placeholder.push(num[indexOfDot + 3]);
-  placeholder.push(num[indexOfDot + 4]);
-  placeholder.push(num[indexOfDot + 5]);
-  placeholder.push(num[indexOfDot + 6]);
-  return placeholder.join("");
+  return num.toFixed(5);
 };
 
 export { truncToTwo, truncToFive };
